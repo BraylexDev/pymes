88f95bafe96fd6e6f9485161a49a78cd798a147e
BraylexDev <perezbrayan1104@gmail.com>
2025-08-08 18:37:30 -0500

    commit initial

---
+++ src/app/core/services/empresa.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpresaService {
+  private baseUrl = 'https://pymes.onrender.com/api/empresa';
+
+  constructor(private http: HttpClient) {}
+
+  registrarEmpresa(datos: any): Observable<string> {
+    return this.http.post(`${this.baseUrl}/registrar`, datos, { responseType: 'text' });
+  }
+
+  enviarCorreoResultado(data: { correo: string; puntaje: number; mensaje: string }): Observable<string> {
+    return this.http.post(`${this.baseUrl}/enviar-correo`, data, { responseType: 'text' });
+  }
+
+  // ✅ Implementación real
+  enviarResultadosPorCorreo(datos: {
+    correo: string;
+    nombreParticipante: string;
+    nombreEmpresa: string;
+    puntaje: number;
+    nivel: string;
+    respuestas: any;
+    mensaje: string;
+  }): Observable<any> {
+    return this.http.post(`${this.baseUrl}/enviarResultados`, datos);
+  }
+}
